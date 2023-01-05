fbca0cd0bab5547848da5cc80f27ec40725d5e58
joyride77 <nfive8@gmail.com>
2023-01-05 12:45:43 +0800

    lession day-36 added

---
+++ day-36/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MainMenu from "./Components/MainMenu";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="menu-container">
+        <MainMenu />
+      </div>
+    </div>
+  );
+}
+
+export default App;
