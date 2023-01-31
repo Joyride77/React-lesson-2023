767f5346795e8fef29866eacb3419d3f6c52888e
joyride77 <nfive8@gmail.com>
2023-01-31 11:40:14 +0800

    added update to Admin panel

---
+++ admin-panel-project/fe/src/pages/Users.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UsersTable from '../components/UsersTable'
+
+const Users = () => {
+    return (
+        <div>
+            <h1>Users Page</h1>
+            <UsersTable />
+        </div>
+    )
+}
+export default Users
