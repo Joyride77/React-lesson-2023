621d01b7636328c03d1f87a1c4efe02f250e46b4
joyride77 <nfive8@gmail.com>
2023-02-15 11:22:14 +0800

    added bcrypt

--- day-62-user-crud-admin/be/index.js
+++ day-62-user-crud-admin/be/index.js
@@ -3,9 +3,11 @@ console.log("Day-62 User Login CRUD");
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const PORT = 8080;
+const SALT_ROUNDS = 10;
 
 app.use(cors());
 app.use(express.json());
@@ -54,30 +56,114 @@ app.post("/register", (request, response) => {
       const roleData = JSON.parse(readData);
       const roleName = roleData.filter((role) => role.id === body.role)[0];
 
-      const userData = {
-        ...body,
-        role: roleName,
-      };
+      const userPassword = body.password;
 
-      dataObject.push(userData);
-
-      fs.writeFile(
-        "./data/users.json",
-        JSON.stringify(dataObject),
-        (writeError) => {
-          if (writeError) {
+      bcrypt.genSalt(SALT_ROUNDS, (error, salt) => {
+        if (error) {
+          response.json({
+            status: "Generating salt error",
+            data: [],
+          });
+        }
+        bcrypt.hash(userPassword, salt, (hashError, hashData) => {
+          if (hashError) {
             response.json({
-              status: "Error during file write",
+              status: "Hashing has error",
               data: [],
             });
           }
-          response.json({
-            status: "success",
-            data: dataObject,
-          });
+          console.log("HASH", hashData);
+          const newUser = {
+            firstname: body.firstname,
+            lastname: body.lastname,
+            email: body.email,
+            password: hashData,
+            address: body.address,
+            role: roleName,
+          };
+
+          dataObject.push(newUser);
+
+          fs.writeFile(
+            "./data/users.json",
+            JSON.stringify(dataObject),
+            (writeError) => {
+              if (writeError) {
+                response.json({
+                  status: "Error during file write",
+                });
+              }
+              response.json({
+                status: "success",
+                data: dataObject,
+              });
+            }
+          );
+        });
+      });
+    });
+  });
+});
+
+/// API login
+app.post("/login", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
+  fs.readFile("./data/users.json", "utf-8", (readError, readData) => {
+    if (readError) {
+      response.json({
+        status: "File not found!",
+        data: [],
+      });
+    }
+
+    const usersArrayObject = JSON.parse(readData);
+    const foundUser = usersArrayObject.filter(
+      (user) => body.email === user.email
+    );
+    if (foundUser.length === 0) {
+      response.json({
+        status: "User not found!",
+        data: [],
+      });
+    } else {
+      const foundUserObj = foundUser[0];
+      console.log(foundUserObj);
+
+      const plainPassword = body.password;
+      const savedPassword = foundUserObj.password;
+
+      bcrypt.compare(
+        plainPassword,
+        savedPassword,
+        (compareError, compareResult) => {
+          if (compareError) {
+            response.json({
+              status: "User name or password do not match",
+              data: [],
+            });
+          }
+
+          if (compareResult) {
+            console.log("It matches");
+            response.json({
+              status: "success",
+              data: {
+                email: foundUserObj.email,
+                firstName: foundUserObj.firstname,
+                lastName: foundUserObj.lastname,
+              },
+            });
+          } else {
+            response.json({
+              status: "Username or Password do not match",
+              data: [],
+            });
+          }
         }
       );
-    });
+    }
   });
 });
 
--- day-62-user-crud-admin/fe/src/App.js
+++ day-62-user-crud-admin/fe/src/App.js
@@ -1,18 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import LoginForm from "./components/LoginForm";
 import UserResisterForm from "./components/UserResisterForm";
 import Users from "./components/Users";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
       <h1>Day-62 User Login CRUD</h1>
-      <h5>User Form</h5>
 
       <Routes>
         <Route path="/register" element={<UserResisterForm />} />
         <Route path="/users" element={<Users />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
