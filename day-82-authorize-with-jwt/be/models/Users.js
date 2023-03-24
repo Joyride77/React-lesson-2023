abfca528e2492703a01c9857e31d559d2ec3fd44
joyride77 <nfive8@gmail.com>
2023-03-24 12:15:51 +0800

    fixed day-84 error

--- day-82-authorize-with-jwt/be/models/Users.js
+++ day-82-authorize-with-jwt/be/models/Users.js
@@ -3,38 +3,40 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   firstname: {
     type: String,
-    required: [true, "Enter the first Name"],
+    required: [true, "Enter the First Name"],
   },
+
   lastname: {
     type: String,
-    required: [true, "Enter the last name"],
+    required: [true, "Enter the Last Name"],
   },
+
   email: {
     type: String,
-    require: [true, "Please provide an Email!"],
+    required: [true, "Please provide an Email!"],
     unique: [true, "Email Exist"],
   },
+
   password: {
     type: String,
-    require: [true, "Please provide a password!"],
+    required: [true, "Please provide a password!"],
     unique: false,
   },
+
   phone: {
     type: Number,
     minimum: 0,
   },
+
   address: {
     type: String,
     required: [true, "Enter the address"],
   },
+
   userrole: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "UserRole",
   },
 });
 
-const Users = mongoose.model("Users", UserSchema);
-
-module.exports = Users;
-
-// module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
