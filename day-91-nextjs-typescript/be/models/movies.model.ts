d84d80db8f5c9e9d092f05a5af513ff88e23e166
joyride77 <nfive8@gmail.com>
2023-04-04 12:47:25 +0800

    added day 91 nextjs-typescript

---
+++ day-91-nextjs-typescript/be/models/movies.model.ts
@@ -0,0 +1,53 @@
+import mongoose, { Date, Schema } from "mongoose";
+
+interface IAwards {
+  wins: Number;
+  nominations: Number;
+  text: string;
+}
+
+interface IImdb {
+  rating: Number;
+  votes: Number;
+  id: Number;
+}
+
+interface IViewer {
+  rating: Number;
+  numReviews: Number;
+}
+
+interface ITomatoes {
+  viewer: IViewer;
+  lastUpdated: Date;
+}
+
+interface IMovie {
+  plot: string;
+  genres: [string];
+  runtime: Number;
+  rated: string;
+  cast: [string];
+  num_mflix_comments: Number;
+  poster: string;
+  title: string;
+  fullplot: string;
+  languages: [string];
+  released: Date;
+  directors: [string];
+  writers: [string];
+  awards: IAwards;
+  lastupdated: Date;
+  year: Number;
+  imdb: IImdb;
+  type: {
+    type: string;
+    enum: ["movie"];
+    required: true;
+  };
+  tomatoes: ITomatoes;
+}
+
+const MoviewSchema: Schema = new Schema({});
+
+export default mongoose.model<IMovie>("Movie", MoviewSchema);
