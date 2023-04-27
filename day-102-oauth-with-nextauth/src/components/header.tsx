0970bb84e0a2540bb5799b669b1a73de375f6ae7
joyride77 <nfive8@gmail.com>
2023-04-27 11:35:13 +0800

    added day-103 lesson

--- day-102-oauth-with-nextauth/src/components/header.tsx
+++ day-102-oauth-with-nextauth/src/components/header.tsx
@@ -1,14 +1,29 @@
-import { signIn, useSession } from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react"
+import styles from "@/styles/header.module.css"
 export default function Header() {
     const { data: session, status } = useSession();
+    const loading = status === "loading"
+
+    console.log(session);
+
+
     const handleLogin = (e: any) => {
         e.preventDefault();
-        signIn();
+        signIn()
+    }
+
+    const handleLogout = (e: any) => {
+        e.preventDefault();
+        signOut();
     }
     return (
         <header>
-            <div>
-                <div>
+            <noscript>
+                <style>{`.nojs-show {opacity:1; top:0}`}</style>
+            </noscript>
+            <div className={styles.signedInStatus}>
+                <div className={`nojs-show ${!session && loading ? styles.loading : styles.loaded
+                    }`}>
                     {
                         !session && (
                             <div>
@@ -20,6 +35,21 @@ export default function Header() {
                             </div>
                         )
                     }
+
+                    {session?.user && (
+                        <div>
+                            {session.user.image && (
+                                <span style={{ backgroundImage: `url(${session.user.image})` }}
+                                    className={styles.avatar} />
+                            )}
+                            <span className={styles.signedInText}>
+                                <small>Signed in as</small>
+                                <br />
+                                <strong>{session.user.email || session.user.name}</strong>
+                            </span>
+                            <a href={`/api/auth/signout`} className={styles.button} onClick={handleLogout}>Sign Out</a>
+                        </div>
+                    )}
                 </div>
             </div>
         </header>
